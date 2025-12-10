b168665e4379f57e1a12960af475c5157b915ae5
TiTo2024 <musstafamahmoud2021@gmail.com>
2025-12-10 15:42:22 +0200

    دا اخري1

--- src/DriveOn/Variables.java
+++ src/DriveOn/Variables.java
@@ -17,8 +17,8 @@ public interface Variables {
 
 
     String [] imgs ={"main(1).png","main2.png","main3.png","police(1).png","police(2).png","police(3).png","car(5).png"
-            ,"car(1).png","car(2).png","car(3).png","live(1).png","live(2).png","live(3).png","Play(101).png","Play(3).png"
-            ,"Play(109).png","Play(108).png","Resume.png","pausemenu.png","car(multi).png","x.png","car(multi1).png","car(multi2).png","Document.png","board.png","background.png"};
+            ,"car(1).png","car(2).png","car(3).png","live(1).png","live(2).png","live(3).png","Play(101).png","Play(13).png"
+            ,"Play(109).png","Play(108).png","Resume1.png","pausemenu1.png","car(multi).png","x.png","car(multi1).png","car(multi2).png","Document.png","board.png","background.png"};
     TextureReader.Texture [] gameTexture = new TextureReader.Texture[imgs.length];
     int [] indexImg  =new int[imgs.length];
 }
